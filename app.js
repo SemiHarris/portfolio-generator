00372f7acb6b0f48fde85f9ce5ff344b65225c91
Semi <Semiharris3@gmail.com>
2022-02-03 05:20:55 -0500

    what is happening

---
+++ app.js
@@ -0,0 +1,13 @@
+const generatePage = require('../src/page-template.js');
+
+const fs = require('fs');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [Name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(Name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
